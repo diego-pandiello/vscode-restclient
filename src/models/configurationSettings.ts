f3eecbfc400a11d4e6b2965cb8c9fba92ecb1a35
Diego Pandiello <diego.fernandez@happening.xyz>
2025-11-16 11:05:33 +0000

    feat: update commands and configuration for aws-srp-rest-client

--- src/models/configurationSettings.ts
+++ src/models/configurationSettings.ts
@@ -255,12 +255,12 @@ export class SystemSettings implements IRestClientSettings {
 
     private initializeSettings() {
         const document = getCurrentTextDocument();
-        const restClientSettings = workspace.getConfiguration("rest-client", document?.uri);
+        const restClientSettings = workspace.getConfiguration("aws-srp-rest-client", document?.uri);
         this._followRedirect = restClientSettings.get<boolean>("followredirect", true);
         this._defaultHeaders = restClientSettings.get<RequestHeaders>("defaultHeaders",
-                                                                     {
-                                                                         "User-Agent": "vscode-restclient"
-                                                                     });
+            {
+                "User-Agent": "vscode-aws-srp-rest-client"
+            });
         this._showResponseInDifferentTab = restClientSettings.get<boolean>("showResponseInDifferentTab", false);
         this._requestNameAsResponseTabTitle = restClientSettings.get<boolean>("requestNameAsResponseTabTitle", false);
         this._rememberCookiesForSubsequentRequests = restClientSettings.get<boolean>("rememberCookiesForSubsequentRequests", true);
